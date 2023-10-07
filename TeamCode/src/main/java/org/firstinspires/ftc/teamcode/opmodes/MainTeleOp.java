ec16908266172718dc294816087e917b556b5716
Faris Allafi <farisqaisallafi@gmail.com>
2023-10-07 08:18:50 -0700

    programmed TeleOP for meacum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
@@ -2,9 +2,13 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDrivetrain;
 
 import java.util.List;
 
@@ -18,7 +22,8 @@ public class MainTeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         myTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
+        MecanumDrivetrain drivetrain = new MecanumDrivetrain(hardwareMap, 537.7, 312);
+        GamepadEx Gamepad1 = new GamepadEx(gamepad1);
         hubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : hubs) hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
 
@@ -26,7 +31,11 @@ public class MainTeleOp extends LinearOpMode {
 
         while (opModeIsActive()) {
             for (LynxModule hub : hubs) hub.clearBulkCache();
-
+            drivetrain.run(
+                    Gamepad1.getLeftX(),
+                    Gamepad1.getLeftY(),
+                    Gamepad1.getRightX()
+            );
             myTelemetry.update();
         }
     }
