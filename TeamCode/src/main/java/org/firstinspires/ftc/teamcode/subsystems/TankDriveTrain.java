f8e8322eb7d1309d1d2cd442ae1b25d54d8ef1c9
Faris Allafi <farisqaisallafi@gmail.com>
2023-11-03 19:58:50 -0700

    Use TeleOPFIX2, full best controls

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TankDriveTrain.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TankDriveTrain {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    public TankDriveTrain(HardwareMap hardwareMap) {
+        leftMotor = hardwareMap.get(DcMotor.class, "left");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightd");
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setPower(double straight, double turn) {
+        leftMotor.setPower(straight + turn);
+        rightMotor.setPower(straight - turn);
+    }
+
+}
