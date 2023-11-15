60b3e2ec103d4c95aa8a77bb71a86db755a8cfbc
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-15 18:10:50 -0500

    sorry more fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrains/MecanumDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrains/MecanumDrivetrain.java
@@ -61,8 +61,6 @@ public class MecanumDrivetrain {
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
         ));
-
-        resetPosition();
     }
 
     public void readIMU() {
@@ -80,10 +78,6 @@ public class MecanumDrivetrain {
 
     public double getHeading() {
         return AngleUnit.normalizeDegrees(latestIMUReading - headingOffset);
-    }
-
-    public void resetPosition() {
-        for (MotorEx motor : motors) motor.encoder.reset();
     }
 
     public void run(double xCommand, double yCommand, double turnCommand) {
