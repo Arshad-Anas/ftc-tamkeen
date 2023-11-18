50d2cca14c55e79d921b3bf9311e0703a4718ade
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-18 18:36:35 -0500

    controller configurable auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainAuton.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.*;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.List;
+
+@Config
+@Autonomous(group = "23925 Autonomous", preselectTeleOp = "MainTeleOp")
+public class MainAuton extends LinearOpMode {
+
+    MultipleTelemetry myTelemetry;
+    List<LynxModule> hubs;
+    GamepadEx Gamepad1;
+    public static boolean red = true;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Initialize multiple telemetry outputs:
+        myTelemetry = new MultipleTelemetry(telemetry);
+        hubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : hubs) hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+
+        boolean right = true;
+        while (!Gamepad1.isDown(RIGHT_STICK_BUTTON)) {
+            if (Gamepad1.wasJustPressed(DPAD_RIGHT)) right = true;
+            if (Gamepad1.wasJustPressed(DPAD_LEFT)) right = false;
+            if (Gamepad1.wasJustPressed(B)) red = true;
+            if (Gamepad1.wasJustPressed(X)) red = false;
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            for (LynxModule hub : hubs) hub.clearBulkCache();
+
+            myTelemetry.update();
+        }
+    }
+}
